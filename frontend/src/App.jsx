45341e4ce261c798eacebc1470a1e811a2e0868f
softlabxsanjeev1 <Sanjeevyaadav28520@gmail.com>
2024-06-25 07:30:35 +0530

    authentication pages frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/header/Header';
+import Home from './pages/home/Home'
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Verify from './pages/auth/Verify';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/verify' element={<Verify />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/pages/auth/Register.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./auth.css"
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+    return (
+        <div className="auth-page">
+            <div className="auth-form">
+                <h2>Login</h2>
+                <form>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" required />
+
+                    <label htmlFor="email">Email</label>
+                    <input type="email" required />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" required />
+
+                    <button className='common-btn'>Login</button>
+                </form>
+                <p>
+                    Already have an account? <Link to="/login">Login</Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Register
